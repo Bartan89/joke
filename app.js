884d8e205fbfffe1b45e29290ab96ac715d1e2eb
Bartan89 <bart.kuijper89@gmail.com>
2020-07-19 18:08:55 +0200

    joked added ready for Heroku

---
+++ app.js
@@ -0,0 +1,33 @@
+const  express = require('express')
+
+const app = express()
+
+const PORT = 3000
+
+
+const page = `
+
+<style>
+  body {
+    padding: 20px;
+    font-family: arial;
+    background-color: #EBEBE9;
+    color: #3B393A;
+  }
+
+</style>
+<body>
+<header>
+  <h1>A programmer showing emotions</h1>
+</header>
+
+  <p>I just want to wrap you between <i>parentheses</i> and <i>require</i> you...</p>
+</body>
+`
+
+
+app.get('/', (request, response) => {
+  response.send(page)
+})
+
+app.listen(PORT, () => console.log(`listening at port: ${PORT}`))
