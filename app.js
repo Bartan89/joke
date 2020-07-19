812317e5f83530a2dab55eaf6224f8debbdd1f26
Bartan89 <bart.kuijper89@gmail.com>
2020-07-19 19:17:49 +0200

    changed port to include optional env PORT for heroku push

--- app.js
+++ app.js
@@ -2,26 +2,25 @@ const  express = require('express')
 
 const app = express()
 
-const PORT = 3000
 
 
 const page = `
 
 <style>
-  body {
-    padding: 20px;
-    font-family: arial;
-    background-color: #EBEBE9;
-    color: #3B393A;
-  }
+body {
+  padding: 20px;
+  font-family: arial;
+  background-color: #EBEBE9;
+  color: #3B393A;
+}
 
 </style>
 <body>
 <header>
-  <h1>A programmer showing emotions</h1>
+<h1>A programmer showing emotions</h1>
 </header>
 
-  <p>I just want to wrap you between <i>parentheses</i> and <i>require</i> you...</p>
+<p>I just want to wrap you between <i>parentheses</i> and <i>require</i> you...</p>
 </body>
 `
 
@@ -29,5 +28,8 @@ const page = `
 app.get('/', (request, response) => {
   response.send(page)
 })
+
+const PORT = process.env.PORT ||3000
+
 
 app.listen(PORT, () => console.log(`listening at port: ${PORT}`))
